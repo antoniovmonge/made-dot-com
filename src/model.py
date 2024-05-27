e7bfc0026a28c9e2aa747eeec722312e0b7ba145
Antonio Vilchez <antoniovmonge@gmail.com>
2024-05-27 17:46:00 +0200

    add "__gt__" to Batch class to use < / > within the model

--- src/model.py
+++ src/model.py
@@ -11,6 +11,12 @@ class OrderLine:
     qty: int
 
 
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    batch = next(b for b in sorted(batches) if b.can_allocate(line))
+    batch.allocate(line)
+    return batch.reference
+
+
 class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
         self.reference = ref
@@ -23,6 +29,13 @@ class Batch:
         if not isinstance(other, Batch):
             return False
         return other.reference == self.reference
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
 
     def __hash__(self):
         return hash(self.reference)
