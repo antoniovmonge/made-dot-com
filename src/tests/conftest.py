686f6bda6c4f4fe1954c80fe184ac0e57aa52b6d
Antonio Vilchez <antoniovmonge@gmail.com>
2024-06-11 11:45:27 +0200

    pending tests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,8 +1,11 @@
 import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
 
 from src import app, db
+from src.orm import metadata, start_mappers
 
-
+# FIXTURES FROM TDD FLASK DOCKER
 @pytest.fixture(scope="module")
 def test_app():
     app.config.from_object("src.config.TestingConfig")
@@ -16,3 +19,17 @@ def test_database():
     yield db
     db.session.remove()
     db.drop_all()
+
+# FIXTURES FROM PYTHON ARCHITECTURE PATTERNS
+@pytest.fixture
+def in_memory_db():
+    engine = create_engine("sqlite:///:memory:")
+    metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def session(in_memory_db):
+    start_mappers()
+    yield sessionmaker(bind=in_memory_db)()
+    clear_mappers()
